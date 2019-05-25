a6d6138c4732d21ba8627e992509cbb99cfe5320
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-05-25 16:35:56 +0300

    Task03.Exercise02

--- Tester.java
+++ Tester.java
@@ -1,17 +1,8 @@
 public class Tester {
 	public static void main(String[] args) {
-		double m = 150;
-	
-		double convert_m = Money.convertBYRtoUSD(m);
-		Printer.print(convert_m);
+		double a = 5;
+		double b = 3;
 
-		convert_m = Money.convertBYRtoEUR(m);
-		Printer.print(convert_m);
-
-		convert_m = Money.convertUSDtoBYR(m);
-		Printer.print(convert_m);
-
-		convert_m = Money.convertEURtoBYR(m);
-		Printer.print(convert_m);
+		Exchange.swapNumbers(a, b);
 	}
 }
