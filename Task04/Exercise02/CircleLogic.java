de465288138cd5a0459111d67d435f161a90d0fe
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-05-28 18:11:19 +0300

    Task2

--- Task04/Exercise02/CircleLogic.java
+++ Task04/Exercise02/CircleLogic.java
@@ -1,5 +1,5 @@
 public class CircleLogic {
 	public static double calcSquareCircle(double a, double b) {
-		return (a > b ? Math.PI * (a * a - b * b) : Math.PI * (b * b - a * a));
+		return a > b ? Math.PI * (a * a - b * b) : Math.PI * (b * b - a * a);
 	}
 }
