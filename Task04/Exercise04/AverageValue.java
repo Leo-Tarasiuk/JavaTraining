e14686f7e22f0bfe4000df4d6b43add54a6cd34c
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-05-28 18:13:32 +0300

    Task4

--- Task04/Exercise04/AverageValue.java
+++ Task04/Exercise04/AverageValue.java
@@ -1,23 +1,47 @@
 public class AverageValue {
+
+	private static final double Number = 6;
 	public static double calcArithmeticAvarage(int a) {
 		int num1 = a % 10;
-		int num2 = (a / 10) % 10;
-		int num3 = (a / 100) % 10;
-		int num4 = (a / 1000) % 10;
-		int num5 = (a / 10000) % 10;
-		int num6 = a / 100000;
+		a /= 10;
 
-		return (num1 + num2 + num3 + num4 + num5 + num6) / 6.0;
+		int num2 = a % 10;
+		a /= 10;
+
+		int num3 = a % 10;
+		a /= 10;
+
+		int num4 = a % 10;
+		a /= 10;
+
+		int num5 = a % 10;
+		a /= 10;
+
+		int num6 = a % 10;
+		a /= 10;
+
+		return (num1 + num2 + num3 + num4 + num5 + num6) / Number;
 	}
 
 	public static double calcGeometricAvarage(int a) {
 		int num1 = a % 10;
-		int num2 = (a / 10) % 10;
-		int num3 = (a / 100) % 10;
-		int num4 = (a / 1000) % 10;
-		int num5 = (a / 10000) % 10;
-		int num6 = a / 100000;
+		a /= 10;
 
-		return Math.pow(num1 * num2 * num3 * num4 * num5 * num6, 1.0/6);
+		int num2 = a % 10;
+		a /= 10;
+
+		int num3 = a % 10;
+		a /= 10;
+
+		int num4 = a % 10;
+		a /= 10;
+
+		int num5 = a % 10;
+		a /= 10;
+
+		int num6 = a % 10;
+		a /= 10;
+
+		return Math.pow(num1 * num2 * num3 * num4 * num5 * num6, 1.0 / Number);
 	}
 }
