0ee23ff059befe26387691f2b33543b22030e8c5
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-06-01 00:55:22 +0300

    Task05

---
+++ Task05/Printer.java
@@ -0,0 +1,11 @@
+package by.epam.javatraining.tarasiuk.lesson5.task1.view.logic;
+
+public class Printer {
+    public static void print(int value) {
+        System.out.println(value);
+    }
+
+    public static String printException() {
+        return "Invalid value, please repeat";
+    }
+}
