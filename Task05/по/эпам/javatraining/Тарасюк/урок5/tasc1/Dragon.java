945ea4864f89d44b1fc13406a04e5aba23fb712b
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-06-01 00:54:09 +0300

    Task05

---
+++ Task05/по/эпам/javatraining/Тарасюк/урок5/tasc1/Dragon.java
@@ -0,0 +1,26 @@
+package by.epam.javatraining.tarasiuk.lesson5.task1.controller.logic;
+
+import by.epam.javatraining.tarasiuk.lesson5.task1.model.logic.DragonLogic;
+import by.epam.javatraining.tarasiuk.lesson5.task1.view.logic.Printer;
+
+import java.util.Scanner;
+
+public class Dragon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int dragonHeads = 0;
+        int dragonEyes = 0;
+
+        try{
+            int dragonAge = scanner.nextInt();
+            dragonHeads = DragonLogic.calcDragonHeads(dragonAge);
+            dragonEyes = DragonLogic.calcDragonEyes(dragonAge);
+        } catch(Exception e) {
+            Printer.printException();
+        }
+
+        Printer.print(dragonEyes);
+        Printer.print(dragonHeads);
+    }
+}
