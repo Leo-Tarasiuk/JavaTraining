272fc24703072e6d7cbc03bdcdcc428d1230f2e2
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-06-03 15:46:32 +0300

    Task4

--- Задача06/Задача 4/срк/по/эпам/javatraining/Тарасюк/урок6/задача 4/просмотр/logic/Printer.java
+++ Задача06/Задача 4/срк/по/эпам/javatraining/Тарасюк/урок6/задача 4/просмотр/logic/Printer.java
@@ -2,7 +2,7 @@ package by.epam.javatraining.tarasiuk.lesson6.task4.view.logic;
 
 public class Printer {
     public static void print(String msg) {
-        System.out.println(msg);
+        System.out.println("Even or uneven? " + msg);
     }
 
     public static void printException() {
