1b75cf35243cd2e1f9f72fd99705179c89d0df10
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-06-09 22:47:51 +0300

    Task3

---
+++ MainTask01/Задача 3/срк/по/эпам/javatraining/Тарасюк/maintask01/задача3/просмотр/логика/Printer.java
@@ -0,0 +1,7 @@
+package by.epam.javatraining.tarasiuk.maintask01.task3.view.logic;
+
+public class Printer {
+    public static void print(boolean flag) {
+        System.out.println(flag);
+    }
+}
