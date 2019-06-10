5acd8c49eaf5ec77bdbcb745549f82b0c2a24f1c
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-06-10 17:09:45 +0300

    Task7

--- MainTask01/Задача7/срк/по/эпам/javatraining/Тарасюк/maintask01/задача 7/просмотр/logic/Printer.java
+++ MainTask01/Задача7/срк/по/эпам/javatraining/Тарасюк/maintask01/задача 7/просмотр/logic/Printer.java
@@ -3,7 +3,8 @@ package by.epam.javatraining.tarasiuk.maintask01.task7.view.logic;
 public class Printer {
     public static void print(int[] array) {
         for (int temp: array) {
-            System.out.println(temp);
+            System.out.print(temp + " ");
         }
+        System.out.println();
     }
 }
