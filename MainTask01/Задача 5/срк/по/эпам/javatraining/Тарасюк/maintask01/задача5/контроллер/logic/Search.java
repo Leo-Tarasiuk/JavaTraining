affe386244b8d0712acde367ed03eb5716e616e3
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-06-10 17:05:45 +0300

    Task5

--- MainTask01/Задача 5/срк/по/эпам/javatraining/Тарасюк/maintask01/задача5/контроллер/logic/Search.java
+++ MainTask01/Задача 5/срк/по/эпам/javatraining/Тарасюк/maintask01/задача5/контроллер/logic/Search.java
@@ -5,10 +5,8 @@ import by.epam.javatraining.tarasiuk.maintask01.task5.view.logic.Printer;
 
 public class Search {
     public static void main(String[] args) {
-        double[] array = {3, 5, 12, 9, 27, 1};
-
-        int index1 = SearchLogic.linearSearch(array, 12);
-        int index2 = SearchLogic.binarySearch(array, 12);
+        int index1 = SearchLogic.linearSearch(12, 23, 12, 32, 54);
+        int index2 = SearchLogic.binarySearch(12, 23, 12, 32, 54);
 
         Printer.print(index1);
         Printer.print(index2);
