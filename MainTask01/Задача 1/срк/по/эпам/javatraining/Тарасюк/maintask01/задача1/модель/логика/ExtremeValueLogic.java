dea57bc28f9b18a9adbf47d34afe15e6efc929a0
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-06-10 16:57:50 +0300

    Task1

--- MainTask01/Задача 1/срк/по/эпам/javatraining/Тарасюк/maintask01/задача1/модель/логика/ExtremeValueLogic.java
+++ MainTask01/Задача 1/срк/по/эпам/javatraining/Тарасюк/maintask01/задача1/модель/логика/ExtremeValueLogic.java
@@ -1,11 +1,30 @@
 package by.epam.javatraining.tarasiuk.maintask01.task1.model.logic;
 
+/**
+ * This class finds max & min values
+ * development date: 09.06.2019
+ *
+ * @author Tarasiuk Leontiy
+ * @version java "1.8.0_211"
+ */
+
 public class ExtremeValueLogic {
-    public static double findMax(double[] array) {
+
+    /**
+     * Method finds max
+     *
+     * @param array of numbers
+     * @return max value
+     */
+    public static double findMax(double... array) {
+        /*If array is empty, return 0
+        If array contains one element, return this element*/
         if (checkNull(array)) {
             return 0;
+        } else if (checkLength(array)) {
+            return array[0];
         }
-
+        //finds max
         double max = array[0];
         for (double temp : array) {
             if (temp > max) {
@@ -15,11 +34,21 @@ public class ExtremeValueLogic {
         return max;
     }
 
-    public static double findMin(double[] array) {
+    /**
+     * Method finds min
+     *
+     * @param array of numbers
+     * @return min value
+     */
+    public static double findMin(double... array) {
+        /*If array is empty, return 0
+          If array contains one element, return this element*/
         if (checkNull(array)) {
             return 0;
+        } else if (checkLength(array)) {
+            return array[0];
         }
-
+        //finds min
         double min = array[0];
         for (double temp : array) {
             if (temp < min) {
@@ -29,7 +58,23 @@ public class ExtremeValueLogic {
         return min;
     }
 
+    /**
+     * Method checks for an empty array
+     *
+     * @param array of numbers
+     * @return Is the array empty
+     */
     private static boolean checkNull(double[] array) {
         return array.length == 0;
+    }
+
+    /**
+     * Method checks whether one element in the array
+     *
+     * @param array of numbers
+     * @return Is there one element in the array
+     */
+    private static boolean checkLength(double[] array) {
+        return array.length == 1;
     }
 }
