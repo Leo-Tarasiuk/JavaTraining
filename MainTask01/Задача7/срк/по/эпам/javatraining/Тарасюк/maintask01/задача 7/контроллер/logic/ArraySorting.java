54f26c99581a29ea233a644a3ad03f77650ac229
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-06-10 17:09:18 +0300

    Task7

--- MainTask01/Задача7/срк/по/эпам/javatraining/Тарасюк/maintask01/задача 7/контроллер/logic/ArraySorting.java
+++ MainTask01/Задача7/срк/по/эпам/javatraining/Тарасюк/maintask01/задача 7/контроллер/logic/ArraySorting.java
@@ -5,10 +5,25 @@ import by.epam.javatraining.tarasiuk.maintask01.task7.view.logic.Printer;
 
 public class ArraySorting {
     public static void main(String[] args) {
-        int[] array = {12, 13, 4, 10};
+        int[] array1 = {12, 13, 4, 10};
+        int[] array2 = {11, 5, 4, 9};
+        int[] array3 = {34, 23, 18, 6};
+        int[] array4 = {5, 41, 22, 11};
+        int[] array5 = {7, 33, 29, 25};
 
-        ArraySortingLogic.mergeSort(array, array.length);
+        ArraySortingLogic.bubbleSort(array1);
+        Printer.print(array1);
 
-        Printer.print(array);
+        ArraySortingLogic.insertSort(array2);
+        Printer.print(array2);
+
+        ArraySortingLogic.selectSort(array3);
+        Printer.print(array3);
+
+        ArraySortingLogic.qSort(array4);
+        Printer.print(array4);
+
+        ArraySortingLogic.mergeSort(array5.length, array5);
+        Printer.print(array5);
     }
 }
