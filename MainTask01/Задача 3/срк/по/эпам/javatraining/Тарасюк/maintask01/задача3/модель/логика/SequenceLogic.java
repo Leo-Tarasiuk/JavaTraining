19c52d248fbcabb2242fb8ee2eb01b216025400d
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-06-10 17:01:32 +0300

    Task3

--- MainTask01/Задача 3/срк/по/эпам/javatraining/Тарасюк/maintask01/задача3/модель/логика/SequenceLogic.java
+++ MainTask01/Задача 3/срк/по/эпам/javatraining/Тарасюк/maintask01/задача3/модель/логика/SequenceLogic.java
@@ -1,26 +1,60 @@
 package by.epam.javatraining.tarasiuk.maintask01.task3.model.logic;
 
+/**
+ * This class checks if the vector is decreasing or increasing.
+ * development date: 09.06.2019
+ *
+ * @author Tarasiuk Leontiy
+ * @version java "1.8.0_211"
+ */
+
 public class SequenceLogic {
-    public static boolean checkSequence(double[] array) {
+    /**
+     * Method checks if the vector is decreasing or increasing.
+     *
+     * @param array of numbers
+     * @return if the vector is decreasing or increasing.
+     */
+    public static boolean checkSequence(double... array) {
+        /*If array is empty, return false
+        If array contains one element, return false*/
         if (checkNull(array)) {
             return false;
+        } else if (checkLength(array)) {
+            return false;
         }
-
-        boolean flag = true;
+        //Is it a sequence
+        boolean isSequence = true;
         double max = array[0];
         if (max > array[1]) {
-            for (int i = 2; i < array.length && flag; i++) {
-                flag = max > array[i];
+            for (int i = 2; i < array.length && isSequence; i++) {
+                isSequence = max > array[i];
             }
         } else if (max < array[1]) {
-            for (int i = 2; i < array.length && flag; i++) {
-                flag = max < array[i];
+            for (int i = 2; i < array.length && isSequence; i++) {
+                isSequence = max < array[i];
             }
         }
-        return flag;
+        return isSequence;
     }
 
+    /**
+     * Method checks for an empty array
+     *
+     * @param array of numbers
+     * @return Is the array empty
+     */
     private static boolean checkNull(double[] array) {
         return array.length == 0;
+    }
+
+    /**
+     * Method checks whether one element in the array
+     *
+     * @param array of numbers
+     * @return Is there one element in the array
+     */
+    private static boolean checkLength(double[] array) {
+        return array.length == 1;
     }
 }
