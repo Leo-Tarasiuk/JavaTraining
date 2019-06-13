2dc90e044961c827f2dbce650200c81efd58b0d6
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-06-13 14:29:56 +0300

    Task3

--- Task06/Task3/src/by/epam/javatraining/tarasiuk/lesson6/task3/model/FactorialLogic.java
+++ Task06/Task3/src/by/epam/javatraining/tarasiuk/lesson6/task3/model/FactorialLogic.java
@@ -3,16 +3,18 @@ package by.epam.javatraining.tarasiuk.lesson6.task3.model.logic;
 public class FactorialLogic {
     public static int calcFact(int fact) {
         if (checkValid(fact)) {
-            int result = 1;
-            for (int i = 1; i <= fact; i++) {
-                result *= i;
-            }
-            return result;
+            return 0;
         }
-        return 0;
+
+        int result = 1;
+        for (int i = 1; i <= fact; i++) {
+            result *= i;
+        }
+
+        return result;
     }
 
     private static boolean checkValid(int fact) {
-        return fact >= 0;
+        return fact < 0;
     }
 }
