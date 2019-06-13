edc21ca236c8c90e1e0a8fe3ea7ab0067b3814a9
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-06-13 14:16:44 +0300

    Task1

--- Task06/Task1/src/by/epam/javatraining/tarasiuk/lesson6/task1/model/CoinLogic.java
+++ Task06/Task1/src/by/epam/javatraining/tarasiuk/lesson6/task1/model/CoinLogic.java
@@ -5,31 +5,27 @@ import java.util.Random;
 public class CoinLogic {
 
     private static int Eagle;
-    private static int Tale;
 
     public static void calcSide(int shot) {
         if (checkValid(shot)) {
-            Random random = new Random();
-            for (int i = 0; i < shot; i++) {
-                int side = random.nextInt(2);
-                if (side == 0) {
-                    Eagle++;
-                } else {
-                    Tale++;
-                }
+            return;
+        }
+
+        Random random = new Random();
+        for (int i = 0; i < shot; i++) {
+            int side = random.nextInt(2);
+            if (side == 0) {
+                Eagle++;
             }
         }
     }
 
     private static boolean checkValid(int shot) {
-        return shot > 0;
+        return shot <= 0;
     }
 
     public static int getEagle() {
         return Eagle;
     }
 
-    public static int getTale() {
-        return Tale;
-    }
 }
