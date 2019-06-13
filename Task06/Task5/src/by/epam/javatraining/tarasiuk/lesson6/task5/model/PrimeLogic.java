cb067f77b3ad521cce0fab623f86a5217503704b
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-06-13 14:42:32 +0300

    Task5

--- Task06/Task5/src/by/epam/javatraining/tarasiuk/lesson6/task5/model/PrimeLogic.java
+++ Task06/Task5/src/by/epam/javatraining/tarasiuk/lesson6/task5/model/PrimeLogic.java
@@ -2,20 +2,21 @@ package by.epam.javatraining.tarasiuk.lesson6.task5.model.logic;
 
 public class PrimeLogic {
     public static boolean checkPrimeNumber(int number) {
-        if(checkValid(number)) {
-            int count = 0;
-            for (int i = 1; i <= number; i++) {
-                if (number % i == 0) {
-                    count++;
-                }
-            }
-
-            return count == 2;
+        if (checkValid(number)) {
+            return false;
         }
-        return false;
+
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+
+        return count == 2;
     }
 
     private static boolean checkValid(int number) {
-        return number > 0;
+        return number <= 0;
     }
 }
