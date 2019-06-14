1cc0ef617e2227d90309a359fc9ce3e974394431
Leo <34138555+Leo-Tarasiuk@users.noreply.github.com>
2019-06-15 01:12:04 +0300

    MainTask01

--- MainTask01/Задача 1/срк/по/эпам/javatraining/Тарасюк/maintask01/задача1/просмотр/логика/Printer.java
+++
@@ -1,7 +0,0 @@
-package by.epam.javatraining.tarasiuk.maintask01.task1.view.logic;
-
-public class Printer {
-    public static void print(double value) {
-        System.out.println(value);
-    }
-}
